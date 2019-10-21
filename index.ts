b328c299e3bd69e0cc13ca74998ae1fc1931b12c
Anwesh43 <anweshthecool0@gmail.com>
2019-10-21 17:15:26 +0530

    adding DrawingUtil class

--- index.ts
+++ index.ts
@@ -52,3 +52,39 @@ class ScaleUtil {
         return Math.sin(scale * Math.PI / n)
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawCircle(context : CanvasRenderingContext2D, x : number, y : number, r : number) {
+        context.beginPath()
+        context.arc(x, y, r, 0, 2 * Math.PI)
+        context.fill()
+    }
+
+    static drawLineCircle(context : CanvasRenderingContext2D, size : number, scale : number) {
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, 2)
+        const sf : number = ScaleUtil.sinify(scale, 1)
+        const sf1 : number = ScaleUtil.sinify(sc1, 2)
+        DrawingUtil.drawLine(context, 0, 0, size * sf, 0)
+        DrawingUtil.drawCircle(context, size * sf1, 0, size / sizeFactor)
+    }
+
+    static drawLCNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        const gap : number = w / (nodes + 2)
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor
+        context.strokeStyle = foreColor
+        context.fillStyle = foreColor
+        context.save()
+        context.translate(gap * (i + 1), h / 2)
+        DrawingUtil.drawLineCircle(context, gap, scale)
+        context.restore()
+    }
+}
