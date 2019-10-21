c2485924b6f97d1d0999250c155f97d294f04740
Anwesh43 <anweshthecool0@gmail.com>
2019-10-21 16:54:23 +0530

    adding constants

---
+++ index.ts
@@ -0,0 +1,8 @@
+const w : number = window.innerWidth
+const h : number = window.innerHeight
+const scGap : number = 0.02
+const strokeFactor : number = 90
+const sizeFactor : number = 4.5
+const nodes : number = 5
+const foreColor : string = "#01579B"
+const backColor : string = "#bdbdbd"
